7d0f88a6dc7b04ebdb4a67e34a7835be5a5c6212
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2018-12-12 05:32:07 -0800

    removed comma from wyoming, cherry to master

--- src/main/java/com/xyzcorp/MyAvroProducer.java
+++ src/main/java/com/xyzcorp/MyAvroProducer.java
@@ -33,7 +33,7 @@ public class MyAvroProducer {
                         "HI,ID,IL,IN,IA,KS,KY,LA,ME,MD," +
                         "MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ," +
                         "NM,NY,NC,ND,OH,OK,OR,PA,RI,SC," +
-                        "SD,TN,TX,UT,VT,VA,WA,WV,WI,WY,";
+                        "SD,TN,TX,UT,VT,VA,WA,WV,WI,WY";
 
         Random random = new Random();
         AtomicBoolean done = new AtomicBoolean(false);
