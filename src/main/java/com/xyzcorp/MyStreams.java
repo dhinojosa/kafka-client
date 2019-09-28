74a192e326fd138a599d3b7458bb93bb7c38e75c
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-09-28 06:58:18 -0600

    Cleaned up Streams for better look

--- src/main/java/com/xyzcorp/MyStreams.java
+++ src/main/java/com/xyzcorp/MyStreams.java
@@ -1,46 +1,48 @@
 package com.xyzcorp;
 
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.*;
-import org.apache.kafka.streams.kstream.*;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.Produced;
 
-import java.util.Arrays;
 import java.util.Properties;
 
 public class MyStreams {
     public static void main(String[] args) {
         Properties props = new Properties();
         props.put(StreamsConfig.APPLICATION_ID_CONFIG,
-                "my_streams_app");
+            "my_streams_app");
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
-                "localhost:9092");
+            "localhost:9092");
         props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
-                Serdes.String().getClass());
+            Serdes.String().getClass());
         props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
-                Serdes.Integer().getClass());
+            Serdes.Integer().getClass());
 
         StreamsBuilder builder = new StreamsBuilder();
 
         KStream<String, Integer> stream =
-                builder.stream("my_orders");
+            builder.stream("my_orders"); //Key: State, Value: Amount
 
-        KStream<String, Integer> ca =
-                stream.filter((key, value) -> key.endsWith("CA"));
+        //One branch
+        stream.filter((key, value) -> key.equals("CA"))
+              .to("california_state_orders");
 
-        ca.peek((s, n) ->
-                System.out.format("Filter msg: %s, %d\n", s, n))
-          .to("priority_state_orders");
-
+        //Second branch
         stream.groupByKey()
-          .count()
-          .toStream()
-          .peek((s, n) -> System.out.format("Group msg: %s, %d\n", s, n))
-          .to("state_orders_count",
+              .count()
+              .toStream()
+              .peek((key, value) ->
+                  System.out.printf("key: %s, value %d", key, value))
+              .to("state_orders_count",
                   Produced.with(Serdes.String(), Serdes.Long()));
 
-        Topology build = builder.build();
+        Topology topology = builder.build();
 
-        KafkaStreams streams = new KafkaStreams(build, props);
+        KafkaStreams streams = new KafkaStreams(topology, props);
 
         streams.setUncaughtExceptionHandler((t, e) -> e.printStackTrace());
 
