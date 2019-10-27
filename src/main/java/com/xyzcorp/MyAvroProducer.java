7b5559eeee33d9d5e0f79dd0bb2515fb88d28b41
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2019-10-27 05:20:57 -0500

    cleaned Producers imports, cleaned connect rest.http

--- src/main/java/com/xyzcorp/MyAvroProducer.java
+++ src/main/java/com/xyzcorp/MyAvroProducer.java
@@ -4,7 +4,6 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
