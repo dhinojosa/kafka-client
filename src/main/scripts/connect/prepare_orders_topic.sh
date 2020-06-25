d5c9467f20d2fa254ea1a03051a397400e4c57e2
Daniel Hinojosa <dhinojosa@evolutionnext.com>
2020-06-24 21:01:41 -0600

    removed connect content

--- src/main/scripts/connect/prepare_orders_topic.sh
+++
@@ -1,5 +0,0 @@
-export CONFLUENT_HOME=~/confluent-5.0.0
-$CONFLUENT_HOME/bin/kafka-topics \
---create --zookeeper localhost:2181 \
---replication-factor 1 --partitions 3 \
---topic mysql-kafka_order
